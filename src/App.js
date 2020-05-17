8f7dbdf3f86ccd4ac47d04dbc08c3672329edc43
Shantina <dinadamjanovik@gmail.com>
2020-05-17 23:38:53 +0200

    finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Todo from "./Todo";
 
 function App() {
+  let name = "Dina";
+  // we need a list of todo's
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTodos([...todos, input]);
+    setInput("");
+  };
+  const handleKeyDown = (event) => {
+    if (event.keyCode === 13) {
+      handleSubmit(event);
+    }
+  };
+  const deleteTodo = (todoIndex) => {
+    const newTodos = todos.filter((_, index) => index !== todoIndex);
+    console.log(newTodos);
+    setTodos(newTodos);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To-Do App made by {name}</h1>
+      <input
+        type="Add Todo"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        onKeyDown={handleKeyDown}
+      />
+      <br />
+      <button disabled={!input} onClick={handleSubmit}>
+        Add Todo
+      </button>
+
+      <div
+        style={{
+          borderRadius: "10px",
+          backgroundColor: "transparent",
+          border: "1px solid black",
+          height: "350px",
+          overflowY: "scroll",
+          width: "25%",
+          margin: "0 auto",
+          marginTop: "50px",
+          opacity: " 0.9",
+        }}
+      >
+        {todos.map((todo, i = 0) => (
+          <Todo title={todo} key={i} id={i} onDelete={(id) => deleteTodo(id)} />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/Todo.js
@@ -0,0 +1,17 @@
+import React from "react";
+// import logo from './logo.svg';
+import "./App.css";
+
+const Todo = ({ id, title, onDelete }) => {
+  return (
+    <div className="todo" id={id}>
+      <h3>
+        <span>{id + 1}. </span>&nbsp;
+        {title}
+      </h3>
+      <i onClick={() => onDelete(id)} className="far fa-trash-alt"></i>
+    </div>
+  );
+};
+
+export default Todo;
